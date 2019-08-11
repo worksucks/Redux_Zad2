4efbd115b9aa083abfd02ed42ad15d965a6c1eee
Tomasz Reizer <t_reizer@yahoo.com>
2019-08-11 07:15:52 +0200

    changed comments.js and reducer.js

--- comments.js
+++ comments.js
@@ -1,15 +1,38 @@
-function comments(state = [], action) {
+function comments(state = initialState, action) {
   switch(action.type) {
     case ADD_COMMENT:
-      return [{
-        id: action.id,
-        text: action.text,
-        votes: 0
-      }
-      ,...state];
+      return Object.assign({}, state, {
+        comments: [
+          {
+            id: action.id,
+            text: action.text,
+            votes: 0
+          }
+          ,...state]
+      })
     case REMOVE_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.filter(comment => comment.id !== action.id)
+      });
     case EDIT_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.filter(comment => comment.id === action.id)
+        text: action.text,
+      });
     case THUMB_UP_COMMENT:
+      return Objest.assign({}, state, {
+        comments: state.comments.filter(comment => comment.id === action.id),
+        votes: this.state.votes +1
+      });
     case THUMB_DOWN_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.filter(comment => comment.id === action.id),
+        votes: this.state.votes -1
+      });
+
+      default:
+        return state;
   }
 }
+
+export default comments;
